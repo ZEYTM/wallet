f9a110011cae124db8aa9c6002ec382f8bb5c420
Magomed Dzeytov <zeytm2.0@gmail.com>
2024-12-07 14:53:25 +0300

    Пессимистическая блокировка

--- src/main/java/com/zeyt/springboot/wallet/repository/WalletRepository.java
+++ src/main/java/com/zeyt/springboot/wallet/repository/WalletRepository.java
@@ -2,13 +2,17 @@ package com.zeyt.springboot.wallet.repository;
 
 
 import com.zeyt.springboot.wallet.entity.Wallet;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
     @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Wallet> findById(UUID uuid);
 
 }
