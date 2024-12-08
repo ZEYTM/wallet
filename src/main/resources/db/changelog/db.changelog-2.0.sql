9b1399e62e5f7db8d1a84308881a83ecbf277b5b
Magomed Dzeytov <zeytm2.0@gmail.com>
2024-12-09 01:34:54 +0300

    ZEYT3.0

--- src/main/resources/db/changelog/db.changelog-2.0.sql
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -1,6 +1,5 @@
 --liquibase formatted sql
 
 --changeset Dzeytov:3
-INSERT INTO wallet (balance)
-VALUES (3000),
-       (5000);
+INSERT INTO wallet (id, balance)
+VALUES ('32bdd58e-e3dc-4332-9c89-1bdcddc204d7',5000);
